8474665a579c69184ebfcda168d3d01bb0496128
rizalazaria <rizalazaria@student.uns.ac.id>
2023-03-06 10:23:28 +0700

    commit 1

---
+++ modul_01/rizal.py
@@ -0,0 +1,7 @@
+nama_depan = "rizal"
+nama_blk   = "azaria"
+kelas      = "D"
+
+print(nama_depan)
+print("nama saya adalah rizal")
+print(f"nama saya adalah {nama_depan} {nama_blk} dari kelas {kelas}")
