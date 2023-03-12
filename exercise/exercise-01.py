792c9bd3fb4a1259bb023850884445837c716c1e
rizalazaria <rizalazaria@student.uns.ac.id>
2023-03-12 12:43:08 +0700

    exercise-01

---
+++ exercise/exercise-01.py
@@ -0,0 +1,21 @@
+nama                = "rizal hakim azaria"
+nim                 = "I0322109"
+tempat_tinggal      = "Perum purikencana,gumpang,kartasura,sukoharjo"
+visualisasi_kampung = "bersih,dekat pabrik,tidak ada limbah,asap pabrik jarang,tidak ada debu,warganya sehat aman dan tentram"
+harapan             = "sukses,mendapatkan pekerjaan yang layak,dan gaji yang besar"
+
+dict = {'a' : nama , 'b' : nim , 'c' : tempat_tinggal, 'd' : visualisasi_kampung, 'e' : harapan}
+
+
+#hasil
+print("Nama saya adalah", dict['a'])
+print("nim saya adalah", dict['b'])
+print("saya bertempat tinggal di", dict['c'])
+print("perkampungan di rumah saya yaitu", dict['d'])
+print('harapan saya setelah lulus di teknik industri uns adalah', dict['e'])
+
+
+
+
+
+
